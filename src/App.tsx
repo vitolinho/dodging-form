dc49dddeca09196935bd98f14c3f2389bf92d935
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-12-03 00:35:31 +0100

    ✨ add confetti

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, ChangeEvent, useState } from "react"
+import confetti from "canvas-confetti"
 
 function App() {
   const [isValid, setIsValid] = useState(false)
@@ -22,9 +23,42 @@ function App() {
       y: _event.clientY
     })
   }
+  const count = 200
+  const defaults = {
+    origin: { y: 0.7 }
+  }
+
+  function fire(particleRatio: number, opts: confetti.Options | undefined) {
+    confetti({
+      ...defaults,
+      ...opts,
+      particleCount: Math.floor(count * particleRatio)
+    })
+  }
   const handleBtnClicked = () => {
     if (isValid) {
-      alert('cliqué !')
+      fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
+      })
+      fire(0.2, {
+        spread: 60,
+      })
+      fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8
+      })
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2
+      })
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+      })
     }
   }
   useEffect(()=> {
